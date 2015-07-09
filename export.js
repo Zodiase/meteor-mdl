da878bf94a09c72bcb6b6c91533614a0c4aa20f6
Xingchen Hong <hello@xc-h.net>
2015-07-09 14:24:37 -0500

    Attached EnvConfig to componentHandler for public access. So the users can know what happened during the initialization of the package. For example, what patchers are effective. It would also be used as a public interface for changing the configuration on the fly. This, however, would depend on the functionalities supported by individual patchers.

--- export.js
+++ export.js
@@ -2,4 +2,8 @@ if (typeof Package !== 'undefined') {
 	/*global componentHandler:true*/  // Meteor.js creates a file-scope global for exporting. This comment prevents a potential JSHint warning.
 	componentHandler = this.componentHandler;
 	delete this.componentHandler;
+	
+	// Attach EnvConfig to componentHandler for public access.
+	/*global EnvConfig:true*/
+	componentHandler.envConfig = EnvConfig;
 }
