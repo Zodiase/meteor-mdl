5d6337143bca984ef75a844a3cdfc2fd30a25705
Xingchen Hong <hello@xc-h.net>
2015-11-03 13:04:21 -0600

    Updated MDL to 1.0.6. Moved change logs out of readme.

---
+++ ForEachUpdate.md
@@ -0,0 +1,15 @@
+This is a note to myself about the tasks to perform for each update.
+
+1. Copy new files from `google/material-design-lite`.
+2. Bump version in `package.js`.
+3. Run unit tests with `meteor test-packages ./`.
+4. Run integration tests:
+    1. Navigate into each project.
+    2. Update project with `meteor update`.
+    3. Run project with `meteor`.
+    4. Check test results in browser.
+5. Fix bugs if needed.
+6. Update change log in `README.md`.
+7. Push changes in `master`.
+8. Push changes in `release`.
+9. Publish with `meteor publish`.
--- package.js
+++ package.js
@@ -1,41 +1,41 @@
 Package.describe({
-	name: 'zodiase:mdl',
-	version: '1.0.5',
-	// Brief, one-line summary of the package.
-	summary: 'A wrapper package for Google\'s Material Design Lite.',
-	// URL to the Git repository containing the source code for this package.
-	git: 'https://github.com/Zodiase/meteor-mdl.git',
-	// By default, Meteor will default to using README.md for documentation.
-	// To avoid submitting documentation, set this field to null.
-	documentation: 'README.md'
+  name: 'zodiase:mdl',
+  version: '1.0.6',
+  // Brief, one-line summary of the package.
+  summary: 'A wrapper package for Google\'s Material Design Lite.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/Zodiase/meteor-mdl.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('1.1.0.2');
-	
-	api.addFiles([
-		'material.css',
-		'material-icons.css',
-		'envConfigs.js',
-		'material.js',
-		'export.js'
-	], 'client');
-	api.export([
-		"componentHandler",
-		"MDl"
-	], 'client');
-	// Patchers.
-	api.addFiles([
-		'patchers/blaze.js'
-	], 'client');
+  api.versionsFrom('1.1.0.2');
+
+  api.addFiles([
+    'material.css',
+    'material-icons.css',
+    'envConfigs.js',
+    'material.js',
+    'export.js'
+  ], 'client');
+  api.export([
+    "componentHandler",
+    "MDl"
+  ], 'client');
+  // Patchers.
+  api.addFiles([
+    'patchers/blaze.js'
+  ], 'client');
 });
 
 Package.onTest(function (api) {
-	api.use('tinytest');
-	api.use('jquery');
-	api.use('zodiase:mdl');
-	api.addFiles([
-		'tests/export.js',
-		'tests/patcher-blaze.js'
-	], 'client');
+  api.use('tinytest');
+  api.use('jquery');
+  api.use('zodiase:mdl');
+  api.addFiles([
+    'tests/export.js',
+    'tests/patcher-blaze.js'
+  ], 'client');
 });
