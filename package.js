ee29b6bcbdf52e2896adfe43e1f0f62b59430be9
Wilson, Andi <andi.wilson@cbsinteractive.com>
2015-11-07 08:39:03 -0500

    Create bash script to add support for SASS.

--- package.js
+++ package.js
@@ -13,6 +13,11 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.2');
 
+  // SCSS file support.
+  api.use([
+      'fourseven:scss@3.3.3'
+  ]);   
+
   api.addFiles([
     'material.css',
     'material-icons.css',
@@ -28,6 +33,49 @@ Package.onUse(function (api) {
   api.addFiles([
     'patchers/blaze.js'
   ], 'client');
+
+  // SCSS files.
+  var scssFiles = [
+    '_animation.scss',
+    '_badge.scss',
+    '_button.scss',
+    '_card.scss',
+    '_checkbox.scss',
+    '_color-definitions.scss',
+    '_data-table.scss',
+    '_functions.scss',
+    '_grid.scss',
+    '_h5bp.scss',
+    '_icon-toggle.scss',
+    '_layout.scss',
+    '_material-design-lite-grid.scss',
+    '_material-design-lite.scss',
+    '_mega_footer.scss',
+    '_menu.scss',
+    '_mini_footer.scss',
+    '_mixins.scss',
+    '_mobile.scss',
+    '_palette.scss',
+    '_progress.scss',
+    '_radio.scss',
+    '_resets.scss',
+    '_ripple.scss',
+    '_shadow.scss',
+    '_slider.scss',
+    '_snackbar.scss',
+    '_spinner.scss',
+    '_switch.scss',
+    '_tabs.scss',
+    '_textfield.scss',
+    '_tooltip.scss',
+    '_typography.scss',
+    '_variables.scss',
+  ];
+  // Create array of pathnames.
+  scssFiles = prepandPathToFiles(scssFiles, 'sass/');
+  // Add files.
+  api.addFiles(scssFiles, 'client');
+  api.addFiles('material.scss', 'client');  
 });
 
 Package.onTest(function (api) {
@@ -39,3 +87,10 @@ Package.onTest(function (api) {
     'tests/patcher-blaze.js'
   ], 'client');
 });
+
+// Helper function for quick path concatenation.
+function prepandPathToFiles(files, path) {
+  return files.map(function(file) {
+    return path + file;
+  });
+}
