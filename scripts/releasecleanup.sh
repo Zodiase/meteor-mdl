da441d0f5dd95b6fe835b79704f40061b9188326
Xingchen Hong <hello@xc-h.net>
2016-01-26 10:50:55 -0600

    Improve release script.

--- scripts/releasecleanup.sh
+++ scripts/releasecleanup.sh
@@ -2,5 +2,22 @@
 
 cd "$(dirname "$0")"
 cd ../
-rm -f ForEachUpdate.md package.json 
+
+# Make sure we're in `master`.
+git checkout master
+
+# Delete existing `release` branch.
+git branch -D release
+
+# Create new `release` branch (with local changes).
+git checkout -b release
+
+# Remove unnecessary files.
+rm -f ForEachUpdate.md package.json
 rm -rf integration-tests node_modules scripts
+
+# Commit changes.
+git commit -am  "Release cleanup."
+
+# Force push to origin (overwriting existing remote branch).
+git push -f origin release
