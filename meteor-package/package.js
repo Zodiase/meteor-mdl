64c854858996d793bdea16cf95c007d177439677
Xingchen Hong <hello@xc-h.net>
2016-07-20 16:50:50 -0500

    `3.8.0` didn't work.

--- meteor-package/package.js
+++ meteor-package/package.js
@@ -22,7 +22,7 @@ Package.onUse(function (api) {
   api.use([
     'ecmascript',
     'isobuild:compiler-plugin@1.0.0',
-    'fourseven:scss@3.8.0'
+    'fourseven:scss@3.8.0_1'
   ]);
   // `check` package is still in testing phase, lock version to prevent BC.
   api.use('zodiase:check@=0.0.4');
